24201c2a3431b50b0fd4988f5e6094be81947807
Craig Chambers <cchambers@salisbury6c.ac.uk>
2016-03-11 09:16:22 +0000

    Testing new way to get IP

--- ip.py
+++ ip.py
@@ -9,6 +9,7 @@ def run_cmd(cmd):
     return output
 
 ip = run_cmd(cmd)
+ip = ip.encode('utf-8')
 #ip = Popen("ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d -f1", shell=True, stdout=PIPE).communicate()[0]
 print('DEBUG: ', ip)
 
