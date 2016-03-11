27ff032e253f5c6705f5e7eb52fac4eb48902f7b
Craig Chambers <cchambers@salisbury6c.ac.uk>
2016-03-11 08:59:37 +0000

    Added debug messages

--- ip.py
+++ ip.py
@@ -3,8 +3,9 @@ from lcd1602 import LCD1602
 from time import sleep
 
 ip = Popen("ip addr show wlan0 | grep inet | awk '{print $2}' |cut -d -f1", shell=True, stdout=PIPE).communicate()[0]
+print('DEBUG: ', ip)
 
 lcd = LCD1602()
 
-lcd.lcd_string("IP address", lcd.LCD_LINE_1)
-lcd.lcd_string(ip, lcd.LCD_LINE_1)
+#lcd.lcd_string("IP address", lcd.LCD_LINE_1)
+#lcd.lcd_string(ip, lcd.LCD_LINE_1)
