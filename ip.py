2bb3e3c696e21d00692590772587ef9728644797
Craig Chambers <cchambers@salisbury6c.ac.uk>
2016-03-11 09:07:07 +0000

    Testing new way to get IP

--- ip.py
+++ ip.py
@@ -2,7 +2,14 @@ from subprocess import *
 from lcd1602 import LCD1602
 from time import sleep
 
-ip = Popen("ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d -f1", shell=True, stdout=PIPE).communicate()[0]
+cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+def run_cmd(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+
+ip = run_cmd(cmd)
+#ip = Popen("ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d -f1", shell=True, stdout=PIPE).communicate()[0]
 print('DEBUG: ', ip)
 
 lcd = LCD1602()
