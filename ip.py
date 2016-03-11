55941a0a29c10b57378ef74142afc41d26a37ac4
Craig Chambers <cchambers@salisbury6c.ac.uk>
2016-03-11 09:46:39 +0000

    Testing new way to get IP

--- ip.py
+++ ip.py
@@ -1,27 +1,19 @@
 from subprocess import *
 from lcd1602 import LCD1602
 from time import sleep
-
 import socket
 import fcntl
 import struct
-import re
-import smtplib
 
-#extract the ip address (or addresses) from ifconfig
-found_ips = []
-ips = re.findall( r'[0-9]+(?:\.[0-9]+){3}', commands.getoutput("/sbin/ifconfig"))
-for ip in ips:
-  if ip.startswith("255") or ip.startswith("127") or ip.endswith("255"):
-    continue
-  found_ips.append(ip)
-
-def getHwAddr(ifname):
+def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
-    return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
-ip= getHwAddr('wlan0')
+ip= get_ip_address('wlan0')
 #ip = Popen("ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d -f1", shell=True, stdout=PIPE).communicate()[0]
 print('DEBUG: ', ip)
 
