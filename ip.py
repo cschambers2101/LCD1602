83f4ef79d458053c128e1ecf45d005bccaabd4ff
Craig Chambers <cchambers@salisbury6c.ac.uk>
2016-03-14 09:09:04 +0000

    Trying different way to get IP

---
+++ ip.py
@@ -0,0 +1,26 @@
+from subprocess import *
+from lcd1602 import LCD1602
+from time import sleep
+import socket
+import fcntl
+import struct
+
+'''
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+ip= get_ip_address('wlan0')
+'''
+
+ip = Popen("ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d -f1", shell=True, stdout=PIPE).communicate()[0]
+print('DEBUG: ', ip)
+
+lcd = LCD1602()
+
+lcd.lcd_string("IP address", lcd.LCD_LINE_1)
+lcd.lcd_string(ip, lcd.LCD_LINE_2)
